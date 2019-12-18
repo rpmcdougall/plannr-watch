73060461e774d58ac0a8d058a0cd0e9119196595
Ryan McDougall <rp.mcdougall@gmail.com>
2019-12-17 21:05:01 -0600

    wired in sql query

--- src/plannr_watch/watch/watch.clj
+++ src/plannr_watch/watch/watch.clj
@@ -5,9 +5,14 @@
             [overtone.at-at :as at-at]))
 
 
+(defn event-listing
+  "Fetches events from database selecting name and time"
+  []
+  (sql/events-name-time db))
 
 (defn watch-events
   "Watches events in plannr database for upcoming events"
   [tp]
-  (at-at/every 10000 #(println "I am cool!") tp))
+  (at-at/every 10000 #(event-listing) tp))
+
 
